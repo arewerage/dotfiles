2041a79376197eb02b737770802db89e049adee6
arewerage <arewerage@gmail.com>
2022-11-23 04:05:41 +0900

    Add new aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,16 @@
 if status is-interactive
     set fish_greeting
+
     starship init fish | source
     zoxide init fish | source
+
+    export PATH="$HOME/.cargo/bin:$PATH"
+
     set EDITOR nvim
     set VISUAL nvim
+
+    alias ls='exa -al --color=always --group-directories-first'
+    alias la='exa -a --color=always --group-directories-first'
+    alias ll='exa -l --color=always --group-directories-first'
+    alias lt='exa -aT --color=always --group-directories-first'
 end
